d73941b2d75d1d436c68c2da7caca79a8ab0d853
mgabriel29 <marvin.gabriel@gmail.com>
2021-04-16 13:27:55 -0700

    job-trackr revision

---
+++ job-trackr/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+//view imports
+import Portal from './Views/Portal'
+
+function App() {
+  return (
+    <div className="App">
+      <Portal />
+    </div>
+  );
+}
+
+export default App;
---
+++ job-trackr/client/src/Views/Portal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Portal() {
+    return (
+        <div className="portal__container">
+            <h1>Portal</h1>
+            
+        </div>
+    )
+}
+
+export default Portal
