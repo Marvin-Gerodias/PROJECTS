e9deddf242888f374dd724c912370fe0890f1659
mgabriel29 <marvin.gabriel@gmail.com>
2021-04-16 19:47:40 -0700

    job trackr update - revised and rebuilt

--- job-trackr/client/src/App.js
+++ job-trackr/client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { Router, Redirect } from '@reach/router'
 
 //view imports
 import Portal from './Views/Portal'
+import Dashboard from './Views/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <Portal />
+      <Router>
+        <Portal path="/portal" />
+        <Dashboard path="/dashboard/:id" />
+
+        <Redirect from="/" to="portal" noThrow />
+      </Router>
     </div>
   );
 }
--- job-trackr/client/src/Views/Portal.js
+++ job-trackr/client/src/Views/Portal.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+//css
+import '../css/Portal.css'
+
+//components
+import NewUser from '../Components/NewUser'
+import ExistingUsers from '../Components/ExistingUsers'
+
 function Portal() {
     return (
         <div className="portal__container">
-            <h1>Portal</h1>
-            
+
+            <div className="comp__container">
+                <div className="comp1">
+                    <h1>Job Trackr</h1>
+                    <p>Keep track of your job applications in one place. Organize information such as company name, when you applied, job title, and so much more.</p>
+                    <p style={{fontStyle:"italic", fontWeight:"600"}}>Applying for jobs just became organized!</p>
+                </div>
+                <div className="comp2">
+                    <h2>Let's get started!</h2>
+                    <hr/>
+                    <ExistingUsers />
+                    <hr/>
+                    <NewUser />
+                </div>
+
+            </div>
         </div>
     )
 }
